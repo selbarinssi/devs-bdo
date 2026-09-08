bae6bc4d51bb76b2d0dffa8d3272a45bd203916d
selbarinssi <elbarinssis@gmail.com>
2026-09-08 08:00:36 +0100

    Fix tab title, Simple Alchemy yields, add Routines tracker

---
+++ src/routes/routines.tsx
@@ -0,0 +1,13 @@
+import { createFileRoute } from "@tanstack/react-router";
+import { AppShell } from "@/components/app-shell";
+import { Routines } from "@/components/routines";
+
+export const Route = createFileRoute("/routines")({ component: RoutinesPage });
+
+function RoutinesPage() {
+  return (
+    <AppShell eyebrow="Task tracker" title="Routines">
+      <Routines />
+    </AppShell>
+  );
+}
