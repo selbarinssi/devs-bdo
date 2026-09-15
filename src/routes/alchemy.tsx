16dbc39390c7dd0f54c59d921ea728abf661f4ad
selbarinssi <elbarinssis@gmail.com>
2026-09-15 14:42:22 +0100

    Update alchemy.tsx

--- src/routes/alchemy.tsx
+++ src/routes/alchemy.tsx
@@ -1,13 +1,4 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { AlchemyPlanner } from "@/components/alchemy-planner";
-import { AppShell } from "@/components/app-shell";
 
-export const Route = createFileRoute("/alchemy")({ component: AlchemyPage });
-
-function AlchemyPage() {
-  return (
-    <AppShell eyebrow="Harmony Draught Pipeline" title="Alchemy Planner">
-      <AlchemyPlanner />
-    </AppShell>
-  );
-}
+export const Route = createFileRoute("/alchemy")({ component: AlchemyPlanner });
