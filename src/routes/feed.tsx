d1b3b128569bc9d41a0807bc5987cf081a2ef2fc
selbarinssi <elbarinssis@gmail.com>
2026-09-15 14:41:51 +0100

    Update feed.tsx

--- src/routes/feed.tsx
+++ src/routes/feed.tsx
@@ -1,13 +1,4 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { AppShell } from "@/components/app-shell";
 import { HubFeed } from "@/components/hub-feed";
 
-export const Route = createFileRoute("/feed")({ component: FeedPage });
-
-function FeedPage() {
-  return (
-    <AppShell eyebrow="Community" title="Hub Feed">
-      <HubFeed />
-    </AppShell>
-  );
-}
+export const Route = createFileRoute("/feed")({ component: HubFeed });
