012140872da1405ffd83c07fbc946ac66efe4db2
selbarinssi <elbarinssis@gmail.com>
2026-09-15 14:42:06 +0100

    Update routines.tsx

--- src/routes/routines.tsx
+++ src/routes/routines.tsx
@@ -1,13 +1,4 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { AppShell } from "@/components/app-shell";
 import { Routines } from "@/components/routines";
 
-export const Route = createFileRoute("/routines")({ component: RoutinesPage });
-
-function RoutinesPage() {
-  return (
-    <AppShell eyebrow="Task Tracker" title="Routines">
-      <Routines />
-    </AppShell>
-  );
-}
+export const Route = createFileRoute("/routines")({ component: Routines });
