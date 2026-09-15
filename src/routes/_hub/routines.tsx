ba7e0d1871afd45a975945791a5f39011a473937
selbarinssi <elbarinssis@gmail.com>
2026-09-15 15:21:21 +0100

    Update routines.tsx

--- src/routes/_hub/routines.tsx
+++ src/routes/_hub/routines.tsx
@@ -1,4 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { Routines } from "@/components/routines";
 
-export const Route = createFileRoute("/routines")({ component: Routines });
+export const Route = createFileRoute("/_hub/routines")({
+  component: Routines,
+});
