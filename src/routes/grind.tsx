c57dc8423e5cd223993cbecda402e35f3c8de346
selbarinssi <elbarinssis@gmail.com>
2026-09-15 14:41:38 +0100

    Update grind.tsx

--- src/routes/grind.tsx
+++ src/routes/grind.tsx
@@ -1,13 +1,4 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { AppShell } from "@/components/app-shell";
 import { GrindTracker } from "@/components/grind-tracker";
 
-export const Route = createFileRoute("/grind")({ component: GrindPage });
-
-function GrindPage() {
-  return (
-    <AppShell eyebrow="PvE Tracker" title="Grind">
-      <GrindTracker />
-    </AppShell>
-  );
-}
+export const Route = createFileRoute("/grind")({ component: GrindTracker });
