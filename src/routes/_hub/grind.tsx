26574c6e9225c2bc81db6b174f3977ebe893a9d6
selbarinssi <elbarinssis@gmail.com>
2026-09-15 15:20:56 +0100

    Update grind.tsx

--- src/routes/_hub/grind.tsx
+++ src/routes/_hub/grind.tsx
@@ -1,4 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { GrindTracker } from "@/components/grind-tracker";
 
-export const Route = createFileRoute("/grind")({ component: GrindTracker });
+export const Route = createFileRoute("/_hub/grind")({
+  component: GrindTracker,
+});
